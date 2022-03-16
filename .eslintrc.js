8d2861083e5e330c1d84eb1ad3aeba2b90c6169b
Joe Lekie <joe.lekie@umbrexus.com>
2022-03-16 02:23:22 -0500

    eslint fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,22 @@
 module.exports = {
     root: true,
     parser: '@typescript-eslint/parser',
+    parserOptions: {
+      project: [ './tsconfig.json']
+    },
     plugins: [
         '@typescript-eslint'
     ],
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended'
-    ]
+    ],
+    rules: {
+      '@typescript-eslint/no-floating-promises': 2,
+      '@typescript-eslint/no-misused-promises': 2,
+      "@typescript-eslint/no-inferrable-types": 0,
+      "@typescript-eslint/no-empty-function": 1,
+      "@typescript-eslint/no-empty-interface": 0,
+      'no-console': 1
+    }
 }
