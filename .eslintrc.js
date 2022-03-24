49470f3bf58e726abea23647bc325872e8c17428
Joe Lekie <joe.lekie@umbrexus.com>
2022-03-24 16:00:14 -0500

    checkpoint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,24 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      project: [ './tsconfig.json']
-    },
-    plugins: [
-        '@typescript-eslint'
-    ],
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended'
-    ],
-    rules: {
-      '@typescript-eslint/no-floating-promises': 2,
-      '@typescript-eslint/no-misused-promises': 2,
-      "@typescript-eslint/no-inferrable-types": 0,
-      "@typescript-eslint/no-empty-function": 1,
-      "@typescript-eslint/no-empty-interface": 0,
-      'no-console': 1
-    }
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: [ './tsconfig.json' ],
+    tsconfigRootDir: __dirname
+  },
+  plugins: [
+      '@typescript-eslint'
+  ],
+  extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended'
+  ],
+  rules: {
+    '@typescript-eslint/no-floating-promises': 2,
+    '@typescript-eslint/no-misused-promises': 2,
+    "@typescript-eslint/no-inferrable-types": 0,
+    "@typescript-eslint/no-empty-function": 1,
+    "@typescript-eslint/no-empty-interface": 0,
+    'no-console': 1
+  },
+  ignorePatterns: [ '.eslintrc.js' ]
 }
