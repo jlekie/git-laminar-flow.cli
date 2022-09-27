b78b8b405418ac6d6b70f4b4e717a69952ef9e56
Joe Lekie <joe.lekie@umbrexus.com>
2022-09-27 14:11:44 -0500

    checkpoint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // export { loadConfig } from './lib/config';
 // export { loadState } from './lib/state';
 export * from './lib/config';
+export * from './lib/settings';
 export * from './lib/plugin';
 export { BaseCommand, BaseInteractiveCommand } from './commands/common';
 // export { init } from './lib/actions';
